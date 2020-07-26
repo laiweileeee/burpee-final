ee23552eb1e7106222abfd4eea08989b37f22922
Lai Wei Lee <laiwei>
2020-07-26 23:15:22 +0800

    Updates

---
+++ precache-manifest.9594d11c8e97cd9a6e058a2a47535eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b97d22205f0b17c8447d492a19cc43",
+    "url": "/burpee-fin/index.html"
+  },
+  {
+    "revision": "3961c88c136b4fb5f561",
+    "url": "/burpee-fin/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d88b319ca63e48a038f",
+    "url": "/burpee-fin/static/css/main.6674e319.chunk.css"
+  },
+  {
+    "revision": "3961c88c136b4fb5f561",
+    "url": "/burpee-fin/static/js/2.f09d80a9.chunk.js"
+  },
+  {
+    "revision": "6e4733f8889d483c0478280e611387dd",
+    "url": "/burpee-fin/static/js/2.f09d80a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d88b319ca63e48a038f",
+    "url": "/burpee-fin/static/js/main.dee78025.chunk.js"
+  },
+  {
+    "revision": "2ccf7cb3cfb21d2ef1cb",
+    "url": "/burpee-fin/static/js/runtime-main.c53aac39.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burpee-final/precache-manifest.e58a5bc236e52f4aa3f7a63aeb6c569a.js"
+  "/burpee-fin/precache-manifest.9594d11c8e97cd9a6e058a2a47535eb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burpee-final/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burpee-fin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
