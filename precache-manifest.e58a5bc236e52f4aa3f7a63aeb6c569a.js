2e5569ab332e0fc324d9e9f53e179a14178c3a24
Lai Wei Lee <laiwei>
2020-07-26 23:24:27 +0800

    Updates

---
+++ precache-manifest.e58a5bc236e52f4aa3f7a63aeb6c569a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55922b0d399decc96934ef6ec6104cc9",
+    "url": "/burpee-final/index.html"
+  },
+  {
+    "revision": "3961c88c136b4fb5f561",
+    "url": "/burpee-final/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "51b302b77dd1498fc2a3",
+    "url": "/burpee-final/static/css/main.6674e319.chunk.css"
+  },
+  {
+    "revision": "3961c88c136b4fb5f561",
+    "url": "/burpee-final/static/js/2.f09d80a9.chunk.js"
+  },
+  {
+    "revision": "6e4733f8889d483c0478280e611387dd",
+    "url": "/burpee-final/static/js/2.f09d80a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b302b77dd1498fc2a3",
+    "url": "/burpee-final/static/js/main.756e6b42.chunk.js"
+  },
+  {
+    "revision": "fc9c0adbeeb4754631f8",
+    "url": "/burpee-final/static/js/runtime-main.4a54d62c.js"
+  }
+]);
